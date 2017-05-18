0bfebc7827cb74cede5ce5b3f6bf6af8631bedb3
katsuwo <katsuwo7720@gmail.com>
2017-05-18 21:52:13 +0900

    0518

--- Sites/script/CesiumControl.js
+++ Sites/script/CesiumControl.js
@@ -13,7 +13,8 @@
     const CE_modelURL = '../model/simple_model.glb';
 
     function CE_init() {
-		CE_polylines = new Cesium.PolylineCollection(); 
+ 		Cecium.BingMapsApi.defaultKey = "AiFixLCMSXG8FLenChOb7hwMqmCB54v37tSBKf8otN5N2AdNNvmLNxj-D5bT-NFE";
+ 		CE_polylines = new Cesium.PolylineCollection(); 
 		CE_scene = viewer.scene;
 		CE_primitives = CE_scene.primitives;
 		CE_primitives.add(CE_polylines);
